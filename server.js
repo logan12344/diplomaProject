3038f0ef7fe4f1cabf7bbb2a12437c7406489987
g-host-one <valeravip84@gmail.com>
2019-05-11 13:38:40 +0300

    add api global

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ WebServer.use(express.static(__dirname + '/frontend'));
 
 WebServer.get("/", (req, res) => {
     res.render('index.html');
-	
 });
+
+WebServer.get("/api/:method",handle);
 
 WebServer.listen(80, (err) => {
     if (err)
@@ -16,3 +17,15 @@ WebServer.listen(80, (err) => {
 
     console.log("Listening port: 80");
 });
+
+function handle(req,res){
+    [group, method] = req.params.method.split('.',2);
+    if (method){
+        "auth.signIn"
+        console.log(group);
+        console.log(method);
+        res.send("ok");
+    }
+    else
+    res.status(404).end();
+}
