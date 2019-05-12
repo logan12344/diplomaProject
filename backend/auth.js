2fedffa398833478f2734e2ed4b76e360f5f8f5e
g-host-one <valeravip84@gmail.com>
2019-05-12 17:15:43 +0300

    error fix

--- backend/auth.js
+++ backend/auth.js
@@ -20,68 +20,108 @@ exports.exec = (method, params, db, callback) => {
 }
 
 function SignIn(params,db,callback){
-    if (params && params.login && params.passwd)
-    {
-        db.query('SELECT user_id FROM users WHERE user_login = $1 AND user_passwd = $2',
-        [params.login, params.passwd], (error, results) =>{
-            if (error) {
-                callback.json({error: true, result: 'Wrong login or password'});
-            } else {
-                session = uuid();
-                db.query('INSERT INTO active_sessions(user_id, session_id) VALUES ($1,$2)',
-                [results.rows[0].user_id, session], (error, results) => {
-                    if (error){
-                        console.log(error.error);
-                        callback.json({error: true,  result: 'Error SignIn'});
-                    } else {
-                        token = jwt.sign({user_id: session},token_config.secret);
-                        callback.json({error: false, result: {token: token}});
-                    }
-                });
+    if (!(params && params.login && params.passwd)){
+        callback.json({error: true, result: 'Wrong login or password'});
+        return;
+    }
+    
+    db.query('SELECT user_id FROM users WHERE user_login = $1 AND user_passwd = $2',
+    [params.login, params.passwd], (error, results) =>{
+        if (error) {
+            console.log(error);
+            callback.json({error: true, result: 'Error SignIn'});
+            return;
+        }
+
+        if (results.rowCount == 0) {
+            callback.json({error: true, result: 'Wrong login or password'});
+            return;
+        }
+
+        session = uuid();
+        db.query('INSERT INTO active_sessions(user_id, session_id) VALUES ($1,$2)',
+        [results.rows[0].user_id, session], (error, results) => {
+            if (error){
+                console.log(error);
+                callback.json({error: true,  result: 'Error SignIn'});
+                return;
             }
+                
+            jwt.sign({user_id: session},token_config.secret, (error, token) => {
+                if (error){
+                    console.log(error);
+                    callback.json({error: true,  result: 'Error SignIn'});
+                    return;
+                }
+
+                callback.json({error: false, result: {token: token}});
+                return;
+            });
         });
-    } else
-    callback.json({error: true, result: 'Wrong login or password'});
+    });
 }
 
 function SignUp(params,db,callback){
     if (params && params.login && params.passwd){
-        db.query('SELECT user_id FROM users WHERE user_login = $1',
-        [params.login], (error, results) =>{
-            if (error) {
-                console.log(error.error);
+        callback.json({error: true, result: 'Wrong login or password'});
+        return;
+    }
+        
+    db.query('SELECT user_id FROM users WHERE user_login = $1',
+    [params.login], (error, results) =>{
+        if (error) {
+            console.log(error);
+            callback.json({error: true,  result: 'Error creating user'});
+            return;
+        } 
+        
+        if (results.rowCount > 0) {
+            callback.json({error: true,  result: 'User name alredy exsist'});
+            return;
+        } 
+        
+        db.query('INSERT INTO users(user_login, user_passwd) VALUES ($1,$2)',
+        [params.login, params.passwd], (error, results) =>{
+            if (error){
+                console.log(error);
                 callback.json({error: true,  result: 'Error creating user'});
-            } else {
-                if (results.rowCount > 0) {
-                    callback.json({error: true,  result: 'User name alredy exsist'});
-                } else {
-                    db.query('INSERT INTO users(user_login, user_passwd) VALUES ($1,$2)',
-                    [params.login, params.passwd], (error, results) =>{
-                        if (error){
-                            console.log(error.error);
-                            callback.json({error: true,  result: 'Error creating user'});
-                        } else {
-                            SignIn(params,db,callback);
-                        }
-                    });
-                }
+                return;
             }
-        });
-    } else
-    callback.json({error: true, result: 'Wrong login or password'});
+            
+            SignIn(params,db,callback);
+         });
+    });
 }
 
 function LogOut(params,db,callback){
-    if (params && params.token)
-    {
-        data = jwt.verify(params.token, token_config.secret);
-        db.query('DELETE FROM active_sessions WHERE session_id = $1', [data.user_id],
+    if (!(params && params.token)) {
+        callback.json({error: true, result: 'Token not found'});
+        return;
+    }
+        
+    jwt.verify(params.token, token_config.secret, (error, decoded) => {
+        if (error){
+            console.log(error);
+            callback.json({error: true, result: 'Wrong token'});
+            return;
+        }
+
+        db.query('DELETE FROM active_sessions WHERE session_id = $1', [decoded.user_id],
         (error, results) =>{
             if (error){
-                callback.json({error: true, result: 'Wrong token'});
-            } else
-            callback.json({error: false, result: ''});
+                console.log(error);
+                callback.json({error: true, result: 'Error log out'});
+                return;
+            }
+        
+            if (results.rowCount == 0){
+                callback.json({error: true, result: 'Alredy log out'});
+                return;
+            }
+        
+            callback.json({error: false, result: 'Log out successful'});
+            return;
         });
-    } else
-    callback.json({error: true, result: 'Token not found'});
+
+    });
 }
