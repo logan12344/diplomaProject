2f467ae8b3570aaee1e0d666b7da9aa2e407da44
g-host-one <valeravip84@gmail.com>
2019-05-12 20:35:25 +0300

    pib on auth

--- backend/auth.js
+++ backend/auth.js
@@ -25,10 +25,10 @@ function SignIn(params,db,callback){
         return;
     }
     
-    db.query('SELECT user_id FROM users WHERE user_login = $1 AND user_passwd = $2',
+    db.query('SELECT users.user_id, teachers_list.pib  FROM users, teachers_list WHERE user_login = $1 AND user_passwd = $2 AND users.teacher_id = teachers_list.teacher_id',
     [params.login, params.passwd], (error, results) =>{
         if (error) {
-            console.log(error);
+            console.log("SELECT: "+ error);
             callback.json({error: true, result: 'Error SignIn'});
             return;
         }
@@ -39,6 +39,7 @@ function SignIn(params,db,callback){
         }
 
         session = uuid();
+        pib = results.rows[0].pib;
         db.query('INSERT INTO active_sessions(user_id, session_id) VALUES ($1,$2)',
         [results.rows[0].user_id, session], (error, results) => {
             if (error){
@@ -54,7 +55,7 @@ function SignIn(params,db,callback){
                     return;
                 }
 
-                callback.json({error: false, result: {token: token}});
+                callback.json({error: false, result: {token: token, pib: pib}});
                 return;
             });
         });
@@ -62,13 +63,13 @@ function SignIn(params,db,callback){
 }
 
 function SignUp(params,db,callback){
-    if (!(params && params.login && params.passwd)){
-        callback.json({error: true, result: 'Wrong login or password'});
+    if (!(params && params.login && params.passwd && params.teach_id)){
+        callback.json({error: true, result: 'Wrong login or password or teacher id'});
         return;
     }
         
-    db.query('SELECT user_id FROM users WHERE user_login = $1',
-    [params.login], (error, results) =>{
+    db.query('SELECT user_id, teacher_id  FROM users WHERE user_login = $1 OR users.teacher_id = $2',
+    [params.login, params.teach_id], (error, results) =>{
         if (error) {
             console.log(error);
             callback.json({error: true,  result: 'Error creating user'});
@@ -76,20 +77,39 @@ function SignUp(params,db,callback){
         } 
         
         if (results.rowCount > 0) {
+            if (results.rows[0].teacher_id == params.teach_id)
+            {
+                callback.json({error: true,  result: 'Such user alredy exsist'});
+                return;
+            }
             callback.json({error: true,  result: 'User name alredy exsist'});
             return;
         } 
-        
-        db.query('INSERT INTO users(user_login, user_passwd) VALUES ($1,$2)',
-        [params.login, params.passwd], (error, results) =>{
+
+        db.query('SELECT teacher_id  FROM teachers_list WHERE teacher_id = $1',
+        [params.teach_id], (error, results) =>{
             if (error){
                 console.log(error);
                 callback.json({error: true,  result: 'Error creating user'});
                 return;
             }
+
+            if (results.rowCount == 0) {
+                callback.json({error: true,  result: 'Pre-defind techer id not found'});
+                return;
+            }
             
-            SignIn(params,db,callback);
-         });
+            db.query('INSERT INTO users(user_login, user_passwd, teacher_id) VALUES ($1,$2,$3)',
+            [params.login, params.passwd, params.teach_id], (error, results) =>{
+                if (error){
+                    console.log(error);
+                    callback.json({error: true,  result: 'Error creating user'});
+                    return;
+                }
+            
+                SignIn(params,db,callback);
+            });
+        });
     });
 }
 
