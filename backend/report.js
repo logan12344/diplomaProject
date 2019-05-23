5ea82b785b5db8d18de833ed0b643ebed6d16318
g-host-one <valeravip84@gmail.com>
2019-05-23 20:59:11 +0300

    Update report.js

--- backend/report.js
+++ backend/report.js
@@ -47,7 +47,7 @@ function get(params,db,callback){
             //if (decoded.permit > 1 && params.tid)
                // decoded.tid = params.tid
 
-            db.query('SELECT reports.report_id, teachers_list.pib as from, to_list.pib as to \
+            db.query('SELECT reports.report_id, teachers_list.pib as from, to_list.pib as to, \
             reports.file_id, method_materials.file_name\
               FROM  teachers_list, teachers_list as to_list, reports, method_materials WHERE \
               teachers_list.teacher_id = reports.report_from AND \
